ec9f40d102bede44518608637f78c133ce207ace
Pratham82 <mali.prathamesh82@gmail.com>
2020-06-01 13:24:18 +0530

    Objects and properties

---
+++ FreeCodeCamp/Basics Javascript/80. Javascript Objects.js
@@ -0,0 +1,23 @@
+// Basic JavaScript: Build JavaScript Objects
+
+/*
+Make an object that represents a dog called myDog which contains the properties "name" (a string), "legs", "tails" and "friends".
+
+You can set these object properties to whatever values you want, as long as "name" is a string, "legs" and "tails" are numbers, and "friends" is an array.
+
+*/
+
+
+var myDog = {
+    // Only change code below this line
+    "name": "lex",
+    "legs":4,
+    "tails":1,
+    "friends":["Joey","Ross","Chandler"]
+    
+    
+    // Only change code above this line
+    };
+    
+
+console.log(myDog);
