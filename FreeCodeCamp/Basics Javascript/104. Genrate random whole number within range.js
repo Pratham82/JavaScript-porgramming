d58e00fb40bf13d721e080c92a626905cfa69a6e
Pratham82 <mali.prathamesh82@gmail.com>
2020-06-03 12:23:37 +0530

    Basics JavaScript challenges completed

---
+++ FreeCodeCamp/Basics Javascript/104. Genrate random whole number within range.js
@@ -0,0 +1,14 @@
+// Basic JavaScript: Generate Random Whole Numbers within a Range
+
+// Create a function called randomRange that takes a range myMin and myMax and returns a random number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+
+
+
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1))+myMin;
+    // Only change code above this line
+}
+
+randomRange(45,46);
+  
---
+++ FreeCodeCamp/Basics Javascript/110. Recursion for creating reange of numbers.js
@@ -0,0 +1,23 @@
+// Basic JavaScript: Use Recursion to Create a Range of Numbers
+
+/*
+We have defined a function named rangeOfNumbers with two parameters. The function 
+should return an array of integers which begins with a number represented by the
+startNum parameter and ends with a number represented by the endNum parameter. 
+The starting number will always be less than or equal to the ending number. 
+Your function must use recursion by calling itself and not use loops of any kind. 
+It should also work for cases where both startNum and endNum are the same.
+*/
+
+function rangeOfNumbers(startNum, endNum) {
+    var myArray =[]
+    if(startNum == endNum){
+        return myArray =[startNum]
+    }else{
+       myArray = rangeOfNumbers(startNum,endNum - 1)
+       myArray.push(endNum)
+       return myArray
+    }
+};
+
+console.log(rangeOfNumbers(4,10));
