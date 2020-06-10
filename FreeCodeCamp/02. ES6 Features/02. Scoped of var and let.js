093bbb2a48200630e7b4198d94ddea0a09c13088
Pratham82 <mali.prathamesh82@gmail.com>
2020-06-10 14:17:01 +0530

    reformatted file

--- FreeCodeCamp/02. ES6 Features/02. Scoped of var and let.js
+++ FreeCodeCamp/02. ES6 Features/02. Scoped of var and let.js
@@ -1,12 +1,11 @@
 // ES6: Compare Scopes of the var and let Keywords
 
-// When you declare a variable with the var keyword, it is declared globally, or 
+// When you declare a variable with the var keyword, it is declared globally, or
 // locally if declared inside a function.
 
 // The let keyword behaves similarly, but with some extra features. When you declare
-// a variable with the let keyword inside a block, statement, or expression, its 
+// a variable with the let keyword inside a block, statement, or expression, its
 // scope is limited to that block, statement, or expression.
-
 
 /*
 Fix the code so that i declared in the if statement is a separate variable than 
@@ -22,14 +21,14 @@ keywords assign scope to the declared variable. When programming a function simi
 // Here both the i's have different value because they are declared different scopes.
 // This shows how thw block level scope and function scope differs.
 function checkScope() {
-    'use strict';
-    let i = 'function scope';
-    if (true) {
-     let i = 'block scope';
-      console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
-  }
-  
-  console.log(checkScope());
+	"use strict";
+	let i = "function scope";
+	if (true) {
+		let i = "block scope";
+		console.log("Block scope i is: ", i);
+	}
+	console.log("Function scope i is: ", i);
+	return i;
+}
+
+console.log(checkScope());
