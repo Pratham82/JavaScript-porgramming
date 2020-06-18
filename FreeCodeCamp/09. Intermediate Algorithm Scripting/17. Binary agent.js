76863f6aad4a63f2c723fabd427e5db9b5d35033
Pratham82 <mali.prathamesh82@gmail.com>
2020-06-18 19:43:39 +0530

    Binary to string convertor

---
+++ FreeCodeCamp/09. Intermediate Algorithm Scripting/17. Binary agent.js
@@ -0,0 +1,34 @@
+// Intermediate Algorithm Scripting: Binary Agents
+
+/*
+Return an English translated sentence of the passed binary string.
+*/
+
+//Solution with ES6
+const binaryAgent = (str) =>
+	str
+		.split(" ")
+		.map((i) => String.fromCharCode(parseInt(i, 2)))
+		.join("");
+
+binaryAgent(
+	"01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+);
+
+console.log(
+	binaryAgent(
+		"01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+	)
+);
+
+// testing solution in normal loop
+var test =
+	"01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111";
+
+var test_arr = test.split(" ");
+var fin_str = "";
+for (var i in test_arr) {
+	fin_str += String.fromCharCode(parseInt(test_arr[i], 2));
+}
+
+console.log(fin_str);
