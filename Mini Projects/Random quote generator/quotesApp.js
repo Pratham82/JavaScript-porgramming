59faa60b0df3caf4cb2fcd0d3e52738c2c6460d2
Pratham82 <mali.prathamesh82@gmail.com>
2020-06-20 09:41:31 +0530

    cleaned up the alt resources

--- Mini Projects/Random quote generator/quotesApp.js
+++ Mini Projects/Random quote generator/quotesApp.js
@@ -6,16 +6,6 @@ generateQuote.addEventListener("click", function () {
 	body.style.background = getRandomColor();
 });
 
-/*
-Alternative resources
-
-
-
-326 quotes
-targetUrl = `https://gist.githubusercontent.com/Pratham82/33900133b2f2fa1181701715120880dd/raw/be4bf5a715f3e2af06a844797cee21ea648f6281/quotes.json`; 
-
-var targetUrl = `https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json`;
-*/
 var proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 targetUrl = `https://type.fit/api/quotes`;
@@ -26,17 +16,9 @@ const getQuotes = () => {
 			return response.json();
 		})
 		.then(function (data) {
-			//For typefit api
 			var randomNum = Math.floor(Math.random() * 1643);
 			quote.innerHTML = data[randomNum]["text"];
-			author_.innerHTML = data[randomNum]["author"];
-
-			// var randomNum = Math.floor(Math.random() * 326);
-			// quote.innerHTML = data[randomNum]["quote"];
-			// author_.innerHTML = data[randomNum]["author"];
-			// console.log(data.length);
-			// console.log(data[randomNum]["quote"], data[randomNum]["author"]);
-			//console.log(data);
+			author_.innerHTML = "&nbsp;- " + data[randomNum]["author"];
 		});
 };
 generateQuote.addEventListener("click", getQuotes);
@@ -51,23 +33,21 @@ function getRandomColor() {
 }
 
 /*
-iteration 1
+Alternative resources for quotes
+
+326 quotes
+targetUrl = `https://gist.githubusercontent.com/Pratham82/33900133b2f2fa1181701715120880dd/raw/be4bf5a715f3e2af06a844797cee21ea648f6281/quotes.json`; 
+
+var targetUrl = `https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json`;
 
 
+code for Alternative resources:
 
-
-fetch(proxyUrl + targetUrl)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
+var randomNum = Math.floor(Math.random() * 326);
+			quote.innerHTML = data[randomNum]["quote"];
+			author_.innerHTML = data[randomNum]["author"];
+			console.log(data.length);
+			console.log(data[randomNum]["quote"], data[randomNum]["author"]);
 			console.log(data);
-		});
-*/
 
-/*
-
-return fetch(`http://jsonplaceholder.typicode.com/posts`)
-		.then((res) => res.json())
-		.then((posts) => console.log(posts));
 */
