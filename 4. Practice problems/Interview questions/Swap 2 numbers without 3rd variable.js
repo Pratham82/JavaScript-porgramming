280af73d37fa6192e1612fe53057d2e7e56c821a
pratham82 <mali.prathamesh82@gmail.com>
2021-01-29 22:25:39 +0530

    Added on new question

---
+++ 4. Practice problems/Interview questions/Swap 2 numbers without 3rd variable.js
@@ -0,0 +1,9 @@
+let num1 = 10
+let num2 = 15
+
+num1 = num1 + num2 // num1 = 35
+num2 = num1 - num2 // num2 = 35 - 15 i.e 10
+num1 = num1 - num2 // num1 = 35 - 10 i.e 15
+
+console.log(`num1: ${num1}`)
+console.log(`num2: ${num2}`)
