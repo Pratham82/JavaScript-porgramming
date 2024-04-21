b296e97b0a102284ee8769a0229d064e4c672048
Pratham82 <prathamesh.mali@mosaicwellness.in>
2024-04-21 17:14:36 +0530

    feat: 🎸 added html sandbox
    added html sandbox
    
    ✅ Closes: 000

---
+++ 2024 Prep/index.js
@@ -0,0 +1,9 @@
+{
+  var a = 10
+  let b = 100
+  const c = 1000
+}
+
+console.log(a)
+// console.log(b) // Reference Error since variable declare with let is block scoped
+// console.log(c) // Reference Error since variable declare with const is block scoped
