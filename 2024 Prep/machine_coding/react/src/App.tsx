3161c12187bc928278817c46500ab34019b04d1f
Pratham82 <prathamesh.mali@mosaicwellness.in>
2024-05-08 12:51:36 +0530

    feat: 🎸 added states batching example

--- 2024 Prep/machine_coding/react/src/App.tsx
+++ 2024 Prep/machine_coding/react/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
-import { FileExplorer } from './components'
-import { fileExplorerData } from './data'
+import { BatchingStates } from './components'
 
 function App() {
   return (
     <div className="container">
       <h1>Vite + React</h1>
-      <div>
-        <FileExplorer data={fileExplorerData} />
-      </div>
+      <div>{/* <FileExplorer data={fileExplorerData} /> */}</div>
+      <BatchingStates />
     </div>
   )
 }
--- 2024 Prep/machine_coding/react/src/components/index.ts
+++ 2024 Prep/machine_coding/react/src/components/index.ts
@@ -1,5 +1,6 @@
 import Loader from './Loader'
 import InfiniteScroll from './InfiniteScroll'
 import FileExplorer from './FileExplorer'
+import BatchingStates from './BatchingStates'
 
-export { Loader, FileExplorer, InfiniteScroll }
+export { Loader, FileExplorer, InfiniteScroll, BatchingStates }
