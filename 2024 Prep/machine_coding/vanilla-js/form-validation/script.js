8cf6f4f20cd45be14b6cab9ca52294a53dc50464
Pratham82 <prathamesh.mali@mosaicwellness.in>
2024-05-12 16:00:26 +0530

    feat: 🎸 added form validation in vanilla js

---
+++ 2024 Prep/machine_coding/vanilla-js/form-validation/script.js
@@ -0,0 +1,20 @@
+const firstName = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const myForm = document.getElementById('my-form')
+
+const onFormSubmit = event => {
+  // * prevent form submit
+  event.preventDefault()
+  // Note: It’s important to prevent the default action of the submit event to avoid triggering a page reload. To do this, simply call event.preventDefault() inside the onFormSubmit function.
+
+  const data = new FormData(event.target)
+  // const dataObject = Object.fromEntries(data.entries())
+
+  const name = data.get('name')
+  const email = data.get('email')
+  const password = data.get('password')
+  console.log({ name, email, password })
+}
+
+myForm.addEventListener('submit', onFormSubmit)
